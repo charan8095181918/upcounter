43998380570cff4f3e9ebd962e94690fa4719d30
charan8095181918 <60030130+charan8095181918@users.noreply.github.com>
2022-04-04 12:07:13 +0530

    Add files via upload

---
+++ up_counter.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:27:43 04/04/2022 
+// Design Name: 
+// Module Name:    up_counter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module up_counter(input clk,input reset,output[3:0] counter);
+reg[3:0] counter_up;
+always @(posedge clk or posedge reset)
+begin
+	if(reset)
+	  counter_up<=4'd0;
+        else
+          counter_up<=counter_up+4'd1;
+        end
+assign counter=counter_up;
+endmodule
